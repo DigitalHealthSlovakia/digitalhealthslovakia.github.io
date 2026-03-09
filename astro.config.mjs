91f1bd396da9b995558f0ad48015248082447dfa
Michal Juhás <michal.juhas@digitalhealthslovakia.com>
2026-03-09 13:48:13 -0400

    Fix internal routing for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config'; // <--- THIS LINE IS THE KEY
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://digitalhealthslovakia.github.io',
-  base: '/DHS_website/',
-  output: 'static',
+  base: '/DHS_website/', 
+  trailingSlash: 'always',
+  build: {
+    format: 'directory'
+  },
   integrations: [mdx(), sitemap()],
 });
